f8ad1232a836ebc5fdecbce3ba4267e017bb4fa1
Ket-Kip <ketcybersecure@gmail.com>
2023-05-10 08:27:41 +0300

    created all the routes

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:index, :show, :create]
+  resources :menu_items, only: [:index, :show, :create, :update, :destroy]
+  resources :menus, only: [:index, :show, :create, :update, :destroy] do
+    # nested resource for menu_items
+    resources :menu_items, only: [:index, :show, :create, :update, :destroy]
+  end
+  resources :order_items
+  resources :orders
+  
+  post '/login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get "/me", to: "users#show"
+  post "/signup", to: "users#create"
 end
